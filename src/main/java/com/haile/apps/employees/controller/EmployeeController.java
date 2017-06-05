dca2fd862ae2fdfb313dd595b32d8e8297736024
haile <hailemichael.nigatu@swedwise.com>
2017-06-05 15:56:31 +0200

    employees path added to EmployeeController.java

--- src/main/java/com/haile/apps/employees/controller/EmployeeController.java
+++ src/main/java/com/haile/apps/employees/controller/EmployeeController.java
@@ -24,37 +24,37 @@ public class EmployeeController {
 	@Autowired
 	 private EmployeeDAO employeeDAO;
 	 	 
-	 @RequestMapping(value= { "/" }, method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	 @RequestMapping(value= { "/employees" }, method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	 public @ResponseBody List<Employee> getEmployeesList() {
 	     
 	     return employeeDAO.getEmployeesList();
 	 }
 	 
-	 @RequestMapping(value= { "/{id}" }, method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	 @RequestMapping(value= { "/employees/{id}" }, method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	 public @ResponseBody Employee getEmployeeById(@PathVariable("id") Integer id) {
 	     
 	     return employeeDAO.getEmployeeById(id);
 	 }
 	 
-	 @RequestMapping(value= { "/newest" }, method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	 @RequestMapping(value= { "/employees/newest" }, method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	 public @ResponseBody Integer getHighestID() {
 	     
 	     return employeeDAO.getHighestID();
 	 }
 	 
-	 @RequestMapping(value= { "/create" }, method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+	 @RequestMapping(value= { "/employees/create" }, method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
 	 public @ResponseBody Integer createEmployee(@RequestBody Employee employee) {
 		 
 	     return employeeDAO.createEmployee(employee.getName(), employee.getCompany(), employee.getJobTitle());
 	 }
 	 
-	 @RequestMapping(value= { "/update" }, method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+	 @RequestMapping(value= { "/employees/update" }, method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
 	 public @ResponseBody Employee updateEmployee(@RequestBody Employee employee) {
 		 
 	     return employeeDAO.updateEmployee(employee.getId(), employee.getName(), employee.getCompany(), employee.getJobTitle());
 	 }
 	 
-	 @RequestMapping(value= { "/remove/{id}" }, method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE)
+	 @RequestMapping(value= { "/employees/remove/{id}" }, method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE)
 	 public @ResponseBody boolean deleteEmployee(@PathVariable("id") Integer id) {
 		 
 	     return employeeDAO.deleteEmployee(id);
